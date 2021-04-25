5f883d78b329fa23ff31b652cca072bdf6f21ff2
EricvanOs <57840271+EricvanOs@users.noreply.github.com>
2021-04-25 17:24:38 +0200

    again

---
+++ PS/_Various Scripts/_Azure/search Azure Graph.ps1
@@ -0,0 +1,54 @@
+# install if needed
+Install-Module -Name Az.ResourceGraph
+
+# import module
+Import-Module -Name Az.ResourceGraph
+
+# list commands
+Get-Command -Module Az.ResourceGraph
+
+#login
+
+Login-AzAccount
+
+#test
+Search-AzGraph -Query 'Resources | project name, type | limit 5'
+
+
+# Key vaults with subscription name
+$query = @"
+Resources
+| join kind=leftouter (ResourceContainers | where type=='microsoft.resources/subscriptions' | project SubName=name, subscriptionId) on subscriptionId
+| where type == 'microsoft.keyvault/vaults'
+| project type, name, SubName
+"@
+
+Search-AzGraph -Query $query
+
+# List virtual machines with their network interface and public IP
+$query = @"
+Resources
+| where type =~ 'microsoft.compute/virtualmachines'
+| extend nics=array_length(properties.networkProfile.networkInterfaces) 
+| mv-expand nic=properties.networkProfile.networkInterfaces 
+| where nics == 1 or nic.properties.primary =~ 'true' or isempty(nic) 
+| project vmId = id, vmName = name, vmSize=tostring(properties.hardwareProfile.vmSize), nicId = tostring(nic.id) 
+| join kind=leftouter (
+    Resources
+    | where type =~ 'microsoft.network/networkinterfaces'
+    | extend ipConfigsCount=array_length(properties.ipConfigurations) 
+    | mv-expand ipconfig=properties.ipConfigurations 
+    | where ipConfigsCount == 1 or ipconfig.properties.primary =~ 'true'
+    | project nicId = id, publicIpId = tostring(ipconfig.properties.publicIPAddress.id))
+on nicId
+| project-away nicId1
+| summarize by vmId, vmName, vmSize, nicId, publicIpId
+| join kind=leftouter (
+    Resources
+    | where type =~ 'microsoft.network/publicipaddresses'
+    | project publicIpId = id, publicIpAddress = properties.ipAddress)
+on publicIpId
+| project-away publicIpId1
+"@
+
+Search-AzGraph -Query $query
