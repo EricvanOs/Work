606f4acb465f9e5808f85783c1bd0f5d3ede14a2
EricvanOs <57840271+EricvanOs@users.noreply.github.com>
2021-05-02 18:09:24 +0200

    ok

---
+++ KQL/UsingPSandKQL.ps1
@@ -0,0 +1,17 @@
+Connect-AzAccount 
+
+$WorkspaceName = 'LogAnalytics2he'
+$WorkspaceResourceGroupName = 'LogAnalytics'
+$Workspace = Get-AzOperationalInsightsWorkspace -ResourceGroupName $WorkspaceResourceGroupName -Name $WorkspaceName
+
+$Query = @"
+workspace("$WorkspaceName").AzureActivity 
+    | where TimeGenerated > ago(120d) 
+    | where ResourceProviderValue =~ "MICROSOFT.COMPUTE" and OperationNameValue =~ "MICROSOFT.COMPUTE/VIRTUALMACHINES/WRITE" and ActivitySubstatusValue =~ "Created" 
+    | extend ResourceName = iff(isnull(Resource), Properties_d.resource, Resource) 
+    | project TimeGenerated, OperationNameValue, ActivityStatusValue,ActivitySubstatusValue,ResourceName,ResourceGroup,Caller,SubscriptionId
+"@
+
+$QueryResults = Invoke-AzOperationalInsightsQuery -Workspace $Workspace -Query $Query
+
+$QueryResults.Results 
