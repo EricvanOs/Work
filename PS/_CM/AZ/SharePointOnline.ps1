421a1c67551161d22a157523844ffc65f62beb88
EricvanOs <57840271+EricvanOs@users.noreply.github.com>
2021-10-14 16:52:28 +0200

    ok

---
+++ PS/_CM/AZ/SharePointOnline.ps1
@@ -0,0 +1,19 @@
+#if needed
+Install-Module -Name Microsoft.Online.SharePoint.PowerShell 
+
+#if needed
+Update-Module -Name Microsoft.Online.SharePoint.PowerShell
+
+
+Import-Module -Name Microsoft.Online.SharePoint.PowerShell -verbose  #with verbose is necessary
+
+# list of commands in module
+get-command -Module Microsoft.Online.SharePoint.PowerShell
+
+# make connection
+Connect-SPOService -Url https://xhelios-admin.sharepoint.com
+
+
+Get-SPOTenant
+
+Get-SPOSite
