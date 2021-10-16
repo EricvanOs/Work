5d7a70eaa923a3a1996be4ce0ce856424ae32d09
EricvanOs <57840271+EricvanOs@users.noreply.github.com>
2021-10-16 16:44:57 +0200

    ok

--- PS/_CM/H03/13 - Export.ps1
+++ PS/_CM/H03/13 - Export.ps1
@@ -1,10 +1,13 @@
 ﻿New-ADUser -Name 'PietjeP' -SamAccountName 'PietjePuk' -GivenName 'Piet' -Surname 'Puk' -DisplayName 'Pietje Puk' `
-           -Path 'OU=PSTest,DC=Pantheon,DC=somewhere'
+           -Path 'CN=Users,DC=Pantheon,DC=somewhere'
 
-Get-ADUser -Identity  'CN=PietjeP,OU=PSTest,DC=Pantheon,DC=somewhere' | Export-Csv -NoTypeInformation  -Path k:\outad.csv
+Get-ADUser -Identity  'CN=PietjeP,CN=Users,DC=Pantheon,DC=somewhere' | Export-Csv -NoTypeInformation  -Path k:\outad.csv
+
 Remove-ADUser PietjePuk -Confirm:$false
 
-# pas outad-file aan --> eerste kolom naar path en entry aanpassen
+Get-ADUser -Filter "Name -eq 'PietjeP'"
+
+# pas outad-file aan naar wens, letop parameter passing byPropertyName
 Import-Csv -Path K:\outad.csv | New-ADUser
 
 Get-ADUser -Filter "Name -eq 'PietjeP'"
@@ -12,4 +15,5 @@ Get-ADUser -Filter "Name -eq 'PietjeP'"
 Remove-ADUser PietjePuk -Confirm:$false
 
 Get-ChildItem -Path K:\outad.csv | Remove-Item
+
 
