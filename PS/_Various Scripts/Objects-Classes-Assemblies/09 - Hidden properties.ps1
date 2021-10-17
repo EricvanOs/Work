daf07eda3f6a9f92f1ad6933a02d555d0e2c3c9f
EricvanOs <57840271+EricvanOs@users.noreply.github.com>
2021-10-17 20:34:27 +0200

    ok

---
+++ PS/_Various Scripts/Objects-Classes-Assemblies/09 - Hidden properties.ps1
@@ -0,0 +1,25 @@
+class Car
+{
+   # Properties
+   [String] $Color
+   [String] $ModelYear
+   [int] $Distance
+
+   # Method
+   [int] Drive ([int]$miles)
+   {
+      $this.Distance += $miles
+      $this.rides++
+      return $this.Distance
+   }
+
+   # Hidden property of the Drive method
+    hidden [int] $rides = 0
+}
+
+$TestCar = [Car]::new()
+
+$TestCar | Get-Member
+
+#again 
+$TestCar | Get-Member -Force
