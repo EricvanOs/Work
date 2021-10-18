dd2e0f1c6964a39d4b3028ae43499a8c0caca281
EricvanOs <57840271+EricvanOs@users.noreply.github.com>
2021-10-18 19:16:04 +0200

    ok

--- PS/_CM/AZ/05 - CreateVM.ps1
+++ PS/_CM/AZ/05 - CreateVM.ps1
@@ -1,12 +1,16 @@
 # connect and set correct context
 Set-AzContext -SubscriptionId (Get-AzSubscription -TenantId ((Connect-AzAccount).Context.Tenant)).Id
 
+$ResourceGroupName = 'myResourceGroup77788'
+if ($null -eq (Get-AzResourceGroup  | Where-Object{$_.ResourceGroupName -eq $ResourceGroupName})) { New-AzResourceGroup -name $ResourceGroupName -Location westeurope }  
+
+
 #  username and password for the Azure VM
-$cred = Get-Credential
+$cred = Get-Credential -UserName admin3444 
 
 # splatting
 $vmParams = @{
-ResourceGroupName = 'myResourceGroup'
+ResourceGroupName = $ResourceGroupName
 Name = 'TestVM'
 Location = 'westeurope'
 ImageName = 'Win2016Datacenter'
@@ -17,3 +21,7 @@ OpenPorts = 3389
 
 
 New-AzVM @vmParams -WhatIf
+
+
+# cleanup
+Get-AzResourceGroup  | Where-Object{$_.ResourceGroupName -eq $ResourceGroupName} | Remove-AzResourceGroup -Confirm:$false -Force
--- PS/_CM/AZ/10 - NewStorageAccount_and_container.ps1
+++ PS/_CM/AZ/10 - NewStorageAccount_and_container.ps1
@@ -1,25 +1,44 @@
 # connect and set correct context
 Set-AzContext -SubscriptionId (Get-AzSubscription -TenantId ((Connect-AzAccount).Context.Tenant)).Id
 
+$ResourceGroupName = 'myResourceGroup77788'
+$location = 'westeurope'
+$StorageAccountName = 'mystorageaccount77788' 
 
-$storageAccount = New-AzStorageAccount -ResourceGroupName $resourceGroup `
--Name "mystorageaccount" `
--SkuName Standard_LRS `
--Location $location `
+$storage = @{
+    ResourceGroupName = $ResourceGroupName
+    Name = $StorageAccountName
+    Location = $location
+}
+
+# create resourcegroup if needed
+if ($null -eq (Get-AzResourceGroup  | Where-Object{$_.ResourceGroupName -eq $ResourceGroupName})) { New-AzResourceGroup -name $ResourceGroupName -Location westeurope }  
+
+# create storage account
+$storageAccount = New-AzStorageAccount @storage -SkuName Standard_LRS  
+
 $ctx = $storageAccount.Context
 
 # Blobs are always uploaded into a container. You can organize groups of blobs the way you organize your files on your computer in folders.
 
-
 $containerName = "quickstartblobs"
 New-AzStorageContainer -Name $containerName -Context $ctx -Permission blob
 
+# add blob, if needed
+Copy-AzStorageBlob -SrcContainer "sourcecontainername" -SrcBlob "srcblobname" -DestContainer "destcontainername" -DestBlob "destblobname"
+
+
 # You can use the Set-AzStorageAccount cmdlet to modify an Azure Storage account.
 
-Set-AzStorageAccount -ResourceGroupName "MyResourceGroup" -AccountName "mystorageaccount" -Type "Standard_RAGRS"
+Set-AzStorageAccount -ResourceGroupName $ResourceGroupName -AccountName $StorageAccountName -Type "Standard_RAGRS"
 
 
 #cleanup
+Get-AzStorageBlob -Container $containerName -Context $ctx | Remove-AzStorageBlob
+Get-AzStorageContainer -Context $ctx | Remove-AzStorageContainer
+Get-AzStorageAccount -ResourceGroupName $ResourceGroupName | Remove-AzStorageAccount -Force
+Get-AzResourceGroup  | Where-Object{$_.ResourceGroupName -eq $ResourceGroupName} | Remove-AzResourceGroup -Confirm:$false -Force
+
 
 
 
