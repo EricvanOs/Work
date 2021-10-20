0c43161d0411766e7617862be3be59452b117c6d
EricvanOs <57840271+EricvanOs@users.noreply.github.com>
2021-10-20 18:26:46 +0200

    ok

---
+++ PS/_CM/H07/01c- null.ps1
@@ -0,0 +1,15 @@
+Set-Location -Path K:\Temp
+
+New-Item -ItemType File -Name 'sample1.txt' 
+
+# don't show output
+New-Item -ItemType File -Name 'sample2.txt' | Out-Null
+
+[void](New-Item -ItemType File -Name 'sample3.txt')
+
+New-Item -ItemType File -Name 'sample4.txt' > $null
+
+$null = (New-Item -ItemType File -Name 'sample5.txt')
+
+#cleanup 
+Get-Item -Path k:\temp\* -Filter "sample?.txt" | Remove-Item 
---
+++ PS/_Various Scripts/Functions/08 - Building Asynchronous PowerShell Functions.ps1
@@ -0,0 +1,30 @@
+﻿ # Remove-AzVM -Name MYVM -ResourceGroupName MYRG (standard function)
+
+ # Make wrapper
+
+function Remove-MyAzVM
+{
+    param (
+        [Parameter(Mandatory)]
+        [string]$Name,
+
+        [Parameter(Mandatory)]
+        [string]$ResourceGroupName
+    )
+    Remove-AzVm -Name $Name -ResourceGroupName $ResourceGroupName
+}
+
+
+
+# Make Scriptblock
+$scriptBlock = {
+    param ($Name,$ResourceGroupName)
+    Remove-MyAzVm -Name $Name -ResourceGroupName $ResourceGroupName
+    ## Other stuff here
+}
+
+# start as Job (asynchronously)
+Start-Job -ScriptBlock $scriptBlock -ArgumentList @($VMName,$ResourceGroupName)
+
+# start synchronously
+& $scriptBlock -VMName $VMName -ResourceGroupName $ResourceGroupName
