ee08e8a1c30c2bb30bd96aaffbd7cc01c5dbd0c6
EricvanOs <57840271+EricvanOs@users.noreply.github.com>
2021-10-21 10:54:33 +0200

    ok

---
+++ PS/_CM/H12/05 - Compare.ps1
@@ -0,0 +1,18 @@
+Set-Location -Path K:\_CM\H12
+
+# difference
+Compare-Object -ReferenceObject (Get-Content -Path .\Testfile1.txt) -DifferenceObject (Get-Content -Path .\Testfile2.txt)
+
+
+# equal
+$objects = @{
+    ReferenceObject = (Get-Content -Path .\Testfile1.txt)
+    DifferenceObject = (Get-Content -Path .\Testfile2.txt)
+  }
+  Compare-Object @objects -ExcludeDifferent
+
+ 
+  # compare on a property of object
+  Compare-Object -ReferenceObject 'abc' -DifferenceObject 'xyz' -Property Length -IncludeEqual
+
+  
