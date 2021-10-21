e536df868b422b34c2958583a74739f2ea0ad47a
EricvanOs <57840271+EricvanOs@users.noreply.github.com>
2021-10-21 11:40:32 +0200

    ok

---
+++ PS/_CM/H03/09c- AdvancedEnumeration.ps1
@@ -0,0 +1,33 @@
+1..100 | ForEach-Object -Process {if ($_ % 2) { return }  "Hello: $_" } 
+
+# parallel
+1..100 | ForEach-Object -Parallel {if ($_ % 2) { return }  "Hello: $_" }    
+
+# two script blocks
+1..2 | ForEach-Object -Begin { 'begin' } -Process { 'process' }
+
+# begin, process (2 blocks) and end
+1..2 | ForEach-Object -Begin { 'begin' }  -process  { 'process A' }  { 'process B' }  -End { 'end' }
+
+
+# begin, process (2*), after process and end
+1..2 | ForEach-Object -Begin { 'begin' }  -process {  'process A'  ; 'process B' } -RemainingScripts {'after process'}  -End { 'end' }
+
+
+
+# parameter passing
+$test1 = 'TestA'
+1..2 | Foreach-Object -Parallel {
+    $using:test1
+}
+
+# throttle
+$Message = "Output:"
+
+1..8 | ForEach-Object -Parallel {
+    "$using:Message $_"
+    Start-Sleep 3
+} -ThrottleLimit 4
+
+
+
