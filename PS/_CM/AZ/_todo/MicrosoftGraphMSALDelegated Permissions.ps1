f551b59b41c2704e862618225814ec728b0ee398
EricvanOs <57840271+EricvanOs@users.noreply.github.com>
2021-10-22 18:13:11 +0200

    omk

---
+++ PS/_CM/AZ/_todo/MicrosoftGraphMSALDelegated Permissions.ps1
@@ -0,0 +1,21 @@
+
+# zie MicrosoftGraphMSALDelegated Permissions
+# 
+# Install-Module -name MSAL.PS -Force -AcceptLicense
+# Install-Module -name JWTDetails
+
+Import-Module MSAL.PS
+
+$clientID = "3c9176b8-0c77-4c12-b2c2-b446a77bc2ac"
+$tenantID = "c395f110-ab2e-44ab-b096-7000e2511b32"
+
+$myAccessToken = Get-MsalToken -DeviceCode -ClientId $clientID -TenantId $tenantID -RedirectUri "https://localhost" 
+
+Import-Module JWTDetails
+$myAccessToken.AccessToken | Get-JWTDetails
+
+
+
+$myAuthMethods = (
+Invoke-RestMethod -Headers @{Authorization = "Bearer $($myAccessToken.AccessToken)" } -Uri "https://graph.microsoft.com/beta/me/authentication/methods"  -Method Get
+$myAuthMethods
---
+++ PS/_CM/H03/14 - Passthru.ps1
@@ -0,0 +1,13 @@
+# geen informatie in $notepad
+$notepad = Start-Process notepad
+
+# nu wel
+$notepad = Start-Process notepad –PassThru
+
+$notepad.StartTime
+
+$notepad | Stop-Process
+
+
+# set of command which have PassThru as parameter
+Get-Command -CommandType 8 | Where-Object {$_.definition -match ‘passthru’}
