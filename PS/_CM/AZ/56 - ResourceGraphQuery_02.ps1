22b0e8c890b7c7702b9c6f7c0d6bea5281c62ddc
EricvanOs <57840271+EricvanOs@users.noreply.github.com>
2021-10-27 21:15:08 +0200

    ok

---
+++ PS/_CM/AZ/56 - ResourceGraphQuery_02.ps1
@@ -0,0 +1,59 @@
+Search-AzGraph -Query "summarize count () by subscriptionId" | Format-Table
+
+Search-AzGraph -Query "project name, location, type|
+ where type =~ 'Microsoft.Compute/virtualMachines' | order by name desc"
+
+Search-AzGraph -Query "where type =~ 'Microsoft.Compute/virtualMachines' |
+ project name, properties.storageProfile.osDisk.osType | top 3 by name desc"
+
+Search-AzGraph -Query "where tags.Role=~'DC' | project name, tags"
+
+$query = @"
+Resources
+| where type == "microsoft.compute/virtualmachines"
+| project  computerNameOverride = tostring(properties.extended.instanceView.computerName), computerName = properties.osProfile.computerName
+"@
+Search-AzGraph -Query $query
+
+
+$query = @"
+Resources | 
+where type =~ 'Microsoft.Network/publicIPAddresses' |
+where properties.ipConfiguration =~ '' |
+project name, resourceGroup, subscriptionId, location, tags, id
+"@
+Search-AzGraph -Query $query
+
+$query = @"
+Resources
+	| where type =~ 'Microsoft.Compute/virtualMachines'
+	| where properties.osProfile.computerName =~ 'hera2he' or properties.extended.instanceView.computerName =~ 'hera2he'
+	| join (ResourceContainers | where type=='microsoft.resources/subscriptions' | project SubName=name, subscriptionId) on subscriptionId
+    | project VMName = name, CompName = properties.osProfile.computerName, OSType =  properties.storageProfile.osDisk.osType, RGName = resourceGroup, SubName, SubID = subscriptionId
+"@
+Search-AzGraph -Query $query
+
+# default Depth level = 2
+$query = @"
+Resources 
+| where type =~ 'Microsoft.Compute/virtualMachines' 
+| limit 1
+"@
+Search-AzGraph -Query $query | ConvertTo-Json -Depth 100
+
+$query = @"
+Resources
+| where type=~ 'microsoft.compute/virtualmachinescalesets'
+| where name contains 'contoso'
+| project subscriptionId, name, location, resourceGroup, Capacity = toint(sku.capacity), Tier = sku.name
+| order by Capacity desc
+"@
+Search-AzGraph -Query $query
+
+$query = @"
+Resources
+| summarize resourceCount=count() by subscriptionId
+| join (ResourceContainers | where type=='microsoft.resources/subscriptions' | project SubName=name, subscriptionId) on subscriptionId
+| project-away subscriptionId, subscriptionId1
+"@
+Search-AzGraph -Query $query
---
+++ PS/_CM/AZ/58 - Upload_to_BlobStorage.ps1
@@ -0,0 +1,28 @@
+Set-Location -Path C:\Work
+
+#Our source File:
+$file = "C:\Work\SomeData.txt"
+
+#Get the File-Name without path
+$name = (Get-Item $file).Name
+
+#The target URL with SAS Token
+$storageaccount = 'storage2he'
+$container = 'test'
+$SASKey = 'sp=racwdl&st=2021-05-02T10:52:55Z&se=2026-05-02T18:52:55Z&spr=https&sv=2020-02-10&sr=c&sig=82%2BkNHUXSivZy2fLmt6TB2KqfPe2gi%2FK6TcQYttR7Xw%3D'
+$uri = "https://$($storageaccount).blob.core.windows.net/$($container)/$($name)?$($SASkey)"
+
+#Define required Headers
+$headers = @{
+    'x-ms-blob-type' = 'BlockBlob'
+}
+
+#Upload File...
+Invoke-RestMethod -Uri $uri -Method Put -Headers $headers -InFile $name
+
+#Upload File again 
+Invoke-RestMethod -Uri $uri -Method Put -Headers $headers -InFile $name # automatic override will take place
+
+# Delete File...
+Invoke-RestMethod -Uri $uri -Method Delete -Headers $headers -InFile $name   
+
