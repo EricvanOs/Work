aa012b0cf3e10e08375f4342f2312284aebf2043
EricvanOs <57840271+EricvanOs@users.noreply.github.com>
2022-06-11 16:43:46 +0200

    ok

--- PS/WorkInProgress/test2.ps1
+++
@@ -1,8 +0,0 @@
-Set-AzContext -SubscriptionId (Get-AzSubscription -TenantId ((Connect-AzAccount).Context.Tenant)).Id
-
-
-
-
-Get-AzDisk 
-| Select-Object -Property Name,ResourceGroupName,Type,DiskSizeGB,DiskState 
-| Where-Object -Property DiskState -eq “Unattached”
---
+++ PS/_Various Scripts/MS Defender,Sentinel, etc/04 - Defender-Sentinel - PS.ps1
@@ -0,0 +1,15 @@
+# Use az.security module for Microsoft Defender for Cloud
+
+Set-AzContext -SubscriptionId (Get-AzSubscription -TenantId ((Connect-AzAccount).Context.Tenant)).Id
+
+
+Get-AzSecurityAlert | Select-Object -First 1
+
+
+
+# use Az.SecurityInsights module for Microsoft Sentinel
+
+Get-Command -Module Az.SecurityInsights
+
+Get-AzSentinelIncident -ResourceGroupName loganalytics -WorkspaceName loganalytics2li
+
