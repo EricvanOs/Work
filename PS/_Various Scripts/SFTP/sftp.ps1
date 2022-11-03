b78a5c25f46c6d13a55e8c0532f0814d4009eb62
EricvanOs <57840271+EricvanOs@users.noreply.github.com>
2022-11-03 15:21:12 +0100

    ok

--- PS/_Various Scripts/SFTP/sftp.ps1
+++ PS/_Various Scripts/SFTP/sftp.ps1
@@ -1,23 +1,40 @@
+# Install-Module -Name WinSCP 
+
+Get-Command -Module WinSCP
+
 # Capture credentials.
-sftp2he.cursus@sftp2he.blob.core.windows.net
+$Hostname = 'sftp2he.blob.core.windows.net'
+$FTPUsername = 'sftp2he.cursus' 
+$FTPPwd = 'sLERvL3x2uxAfVgVX8snmmoQlCD/0dxU'
+$Password = ConvertTo-SecureString $FTPPwd -AsPlainText -Force
+$Credential = New-Object System.Management.Automation.PSCredential ($FTPUsername, $Password)
 
-sLERvL3x2uxAfVgVX8snmmoQlCD/0dxU
 
-$credential = Get-Credential
+$sessionOption = New-WinSCPSessionOption -HostName $Hostname -Protocol Sftp -Credential $credential
+$SshHostKeyFingerprint = Get-WinSCPHostKeyFingerprint -SessionOption $sessionOption -Algorithm SHA-256
+
 
 # Set the options to open the WinSCPSession with
-$sessionOption = New-WinSCPSessionOption -HostName sftp2he.blob.core.windows.net -Protocol Scp -Credential $credential -GiveUpSecurityAndAcceptAnySshHostKey
+$sessionOption = New-WinSCPSessionOption -HostName $Hostname -Protocol Sftp -Credential $credential -SshHostKeyFingerprint  $SshHostKeyFingerprint
 
 # Open the session using the SessionOptions object.
 # New-WinSCPSession sets the PSDefaultParameterValue of the WinSCPSession parameter for all other cmdlets to this WinSCP.Session object.
 # You can set it to a variable if you would like, but it is only necessary if you will have more then one session open at a time.
-New-WinSCPSession -SessionOption $sessionOption
+$WinSCPSession =  New-WinSCPSession -SessionOption $sessionOption 
 
-# Use that session to create a new Directory.
+# list items
+Get-WinSCPChildItem -WinSCPSession $WinSCPSession
+
+# Create a new Directory. (no rights)
 New-WinSCPItem -Path './remoteDirectory' -ItemType Directory
 
-# Upload a file to the directory.
+# Upload a file to the directory (no rights)
 Send-WinSCPItem -Path 'C:\localDirectory\localFile.txt' -Destination '/remoteDirectory/'
 
+# get a file
+Receive-WinSCPItem -WinSCPSession $WinSCPSession -RemotePath '/Folder2/ReadMe.txt' -LocalPath C:\Temp
 # Close and remove the session object.
-Remove-WinSCPSession
+
+Remove-WinSCPSession -WinSCPSession $WinSCPSession
+
+
