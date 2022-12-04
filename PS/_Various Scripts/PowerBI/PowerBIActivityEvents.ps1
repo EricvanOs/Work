013d43a43363c4bd30838ddf248b2dce82ccb56f
EricvanOs <57840271+EricvanOs@users.noreply.github.com>
2022-12-04 10:17:00 +0100

    ok

--- PS/_Various Scripts/PowerBI/PowerBIActivityEvents.ps1
+++ PS/_Various Scripts/PowerBI/PowerBIActivityEvents.ps1
@@ -3,7 +3,7 @@ Login-PowerBI
 # Only events per day (Could take 24 hours to showup)
 # https://learn.microsoft.com/en-us/power-bi/admin/service-admin-auditing
 
-$StartDateTime = '2022-12-04T00:00:00' 
+$StartDateTime = '2022-12-04T00:00:00'  # use exactly this format (string)
 $EndDateTime  =  '2022-12-04T23:59:59'
 
 $activities = Get-PowerBIActivityEvent -StartDateTime $StartDateTime -EndDateTime $EndDateTime | ConvertFrom-Json
