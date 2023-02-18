74d9a500827051124d40727adfd5e3e33f654366
EricvanOs <57840271+EricvanOs@users.noreply.github.com>
2023-02-18 12:11:17 +0100

    ok

--- PS/_CM/H03B/02 - ByPropertyName.ps1
+++ PS/_CM/H03B/02 - ByPropertyName.ps1
@@ -9,7 +9,7 @@ Get-Help Get-Hotfix -Online  # ByPropertyName  -- ComputerName
 
 Get-HotFix -ComputerName Hydra,Apate
 Get-HotFix -Description Update 
-Get-HotFix -Id KB5013888
+Get-HotFix -Id KB5022504 
 
 
 # maw een computername,id of description als input voor get-hotfix
--- PS/_CM/H03B/05 - ExpandingProperties.ps1
+++ PS/_CM/H03B/05 - ExpandingProperties.ps1
@@ -2,40 +2,40 @@
 # get-service has changed in ps 7
 
 # 1
-Get-ADComputer -Filter ('Name -like "e*us"')
+Get-ADComputer -Filter ('Name -like "*o"')
 
 # goes wrong
-Get-Service -ComputerName (Get-ADComputer -Filter ('Name -like "e*us"'))
+Get-Service -ComputerName (Get-ADComputer -Filter ('Name -like "*o"'))
 
-get-service -ComputerName (Get-ADComputer -Filter ('Name -like "e*us"') | Select-Object -Property Name)
+get-service -ComputerName (Get-ADComputer -Filter ('Name -like "*o"') | Select-Object -Property Name)
 
 # 3
-Get-ADComputer -Filter ('Name -like "e*us"') | Get-Member
+Get-ADComputer -Filter ('Name -like "*o"') | Get-Member
 
 # 4
 Get-Help Get-Service -online #computername should be a string[]
 
 # 5
 
-Get-ADComputer -Filter ('Name -like "e*us"') | Select-Object -Property Name  # merk op een kolom met naam
+Get-ADComputer -Filter ('Name -like "*o"') | Select-Object -Property Name  # merk op een kolom met naam
 
 # 6
-Get-ADComputer -Filter ('Name -like "e*us"') | Select-Object -Property Name | Get-Member # Selected....ADComputer
+Get-ADComputer -Filter ('Name -like "*o"') | Select-Object -Property Name | Get-Member # Selected....ADComputer
 
 # dit werkt niet, omdat 't stuk tussen haakjes een kolom met een naam terug geeft 
-Get-Service -ComputerName (Get-ADComputer -Filter ('Name -like "e*us"') | Select-Object -Property Name)
+Get-Service -ComputerName (Get-ADComputer -Filter ('Name -like "*o"') | Select-Object -Property Name)
 
 # 7 create a column without name. only entries, no header
-Get-ADComputer -Filter ('Name -like "e*us"') | Select-Object -ExpandProperty Name
+Get-ADComputer -Filter ('Name -like "*o"') | Select-Object -ExpandProperty Name
 
 # 8
-Get-ADComputer -Filter ('Name -like "e*us"') | Select-Object -ExpandProperty Name |   Get-Member #type string
+Get-ADComputer -Filter ('Name -like "*o"') | Select-Object -ExpandProperty Name |   Get-Member #type string
 
 # nu werkt 't wel
-Get-Service -ComputerName (Get-ADComputer -Filter ('Name -like "e*us"') | 
+Get-Service -ComputerName (Get-ADComputer -Filter ('Name -like "*o"') | 
   Select-Object -ExpandProperty Name)
 
-Get-Service -ComputerName (Get-ADComputer -Filter ('Name -like "e*us"') | 
+Get-Service -ComputerName (Get-ADComputer -Filter ('Name -like "*o"') | 
   Select-Object -ExpandProperty Name) | Select-Object -Property MachineName,DisplayName,Status
 
 
