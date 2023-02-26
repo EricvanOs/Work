46d2e64a540f693c6197b8ae5f34c10c282908b7
EricvanOs <57840271+EricvanOs@users.noreply.github.com>
2023-02-26 15:11:41 +0100

    ok

--- PS/_Various Scripts/Objects-Classes-Assemblies/00a- Add-Member.ps1
+++ PS/_Various Scripts/Objects-Classes-Assemblies/00a- Add-Member.ps1
@@ -1,38 +1,32 @@
-
+Set-Location -Path C:\Work\PS\Temp
 
 # Add a note property to a PSObject
-
-$A = Get-ChildItem c:\ps-test\test.txt
+$A = Get-ChildItem .\MyTest.txt
 $A | Add-Member -NotePropertyName Status -NotePropertyValue Done
+$A | Get-Member
 $A.Status
 
 
-
 # Add an alias property to a PSObject
-
-$A = Get-ChildItem C:\Temp\test.txt
+$A = Get-ChildItem .\MyTest.txt
 $A | Add-Member -MemberType AliasProperty -Name Size -Value Length
 $A.Size
 
 
-
 # Add a StringUse note property to a string
-
 $A = "A string"
 $A = $A | Add-Member -NotePropertyMembers @{StringUse="Display"} -PassThru
 $A.StringUse
 
 
 # Add a script method to a FileInfo object
-
-$A = Get-ChildItem C:\Temp\test.txt
+$A = Get-ChildItem .\MyTest.txt
 $S = {[math]::Round(($this.Length / 1MB), 2)}
 $A | Add-Member -MemberType ScriptMethod -Name "SizeInMB" -Value $S
 $A.SizeInMB()
 
 
 # Create a custom object
-
 $Asset = New-Object -TypeName PSObject
 $d = [ordered]@{Name="Server30";System="Server Core";PSVersion="4.0"}
 $Asset | Add-Member -NotePropertyMembers $d -TypeName Asset
