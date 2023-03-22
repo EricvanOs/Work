06e17354a1e04572ed78e2d08d38c092e4cd73a1
EricvanOs <57840271+EricvanOs@users.noreply.github.com>
2023-03-22 20:55:16 +0100

    ok

---
+++ PS/Temp/PS/julia/A01 - function.ps1
@@ -0,0 +1,43 @@
+Function SquareRoot {
+    [cmdletbinding()]
+    Param(
+    [Parameter(Position = 0, Mandatory, ValueFromPipeline)]
+    [int[]]$Value
+    )
+
+    Begin {
+        Write-Verbose "[BEGIN ] Starting: $($MyInvocation.Mycommand)"
+    } 
+
+    Process {
+        foreach ($item in $value) {
+            [pscustomobject]@{
+            Value = $item
+            SquareRoot = [math]::Sqrt($item)
+            }
+        }
+    } 
+   
+    End {
+        Write-Verbose "[END ] Ending: $($MyInvocation.Mycommand)"
+    } 
+}
+
+# The command is written so that the user can pipe in a list of numbers or pass the list with the parameter.
+$n = 1..1000
+Measure-Command {$n | SquareRoot}
+Measure-Command {SquareRoot $n }
+
+# In this simple comparison we scored 52ms vs 38ms respectively. Let’s see the difference with varying
+# sets of numbers between using the pipeline and using the parameter.
+10,100,500,1000,5000,10000 | ForEach-Object {
+    $n = 1..$_
+    $pipe = (Measure-Command {$n | squareroot}).totalMilliseconds
+    $param = (Measure-Command {squareroot $n}).TotalMilliseconds
+    [pscustomobject]@{
+    ItemCount = $_
+    PipelineMS = $pipe
+    ParameterMS = $param
+    PctDiff = 100 - (($param/$pipe) * 100 -as [int])
+    }
+}
