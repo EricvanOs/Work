4cd7a81573f3ff74619622f72c3c30426e995b85
EricvanOs <57840271+EricvanOs@users.noreply.github.com>
2023-03-25 10:22:29 +0100

    ok

---
+++ PS/_CM/T07/10-WriteDebug.ps1
@@ -0,0 +1,21 @@
+﻿function Get-FilewithDebug
+{
+    [cmdletbinding()]
+    Param
+    (
+        [parameter(Mandatory)]
+        [string]$path
+    )
+
+    Write-Verbose 'Starting script'
+    Write-Debug "`$path is: $path"
+    $return = Get-ChildItem -Path $path -Filter *.exe -Recurse -Force
+    Write-Debug "`$return has $($return.count) items"
+    $return
+}
+
+
+ Get-FilewithDebug -path C:\Users\administrator.PANTHEON\Downloads -Verbose -Debug
+
+# set-location C:\Work\PS\_CM\H07B
+ # Suspend, test variables, and exit to continue
---
+++ PS/_CM/T07/14 - Debug_Function_WriteLog.ps1
@@ -0,0 +1,31 @@
+﻿function Write-Log {
+    Param(
+        [string]$Folder,
+        [string]$File,
+        [string]$Data
+    )
+
+    #Calculate full path for log files
+    $logPath = Join-Path -Path $folder -ChildPath $file
+    
+    #Calculate time stamp for log entry
+    $date = Get-Date
+    $timeStamp = $date.ToShortDateString() + ' ' + $date.ToLongTimeString() + ' : '
+    
+    #Write data to log file
+    $timeStamp + $data | Out-File $logPath -Append
+}
+
+# use vscode
+# add breakpoint at row 9,16 and 25(click/Toggle Break Point (F9))
+# start with debugging (F5)
+
+Set-Location -Path C:\Work\PS\_CM
+
+Write-Log -Folder . -File TestLog.txt -Data "Test log data" 
+
+Get-Content -Path .\TestLog.txt
+# cleanup
+Remove-Item -Path .\TestLog.txt
+
+Set-Location -Path c:\
