54c235f5e0559f66f512babb63326856f1adc9a2
EricvanOs <57840271+EricvanOs@users.noreply.github.com>
2023-03-26 15:49:06 +0200

    ok

--- PS/_CM/T01/E01 - Splatter.ps1
+++ PS/_CM/T01/E01 - Splatter.ps1
@@ -9,3 +9,25 @@ $pid  #system variable of owner process
 @{Id=$pid;Junk='Data'} | ?@ gps
 # -or
 @{Id=$pid;Junk='Data'} | & ${?@} gps
+
+
+# splat information
+$splat = @{id=$pid;foo='bar'} | Get-Splat Get-Process
+# or
+$splat = @{id=$pid;foo='bar'} | ?@ gps
+$splat.Command, $splat.PercentFit, $splat.Unmapped
+
+
+# merge
+@{a='b'}, @{c='d'} | Merge-Splat
+
+
+# find splat
+@{id=$pid} | Find-Splat *-Process
+
+
+# use-splat
+# Gets the process, and then doesn't stop the process because Stop-Process is passed WhatIf
+@{id=$pid;WhatIf=$true} | Use-Splat Get-Process,Stop-Process 
+# or
+@{id=$pid;WhatIf=$true} | .@ Get-Process,Stop-Process 
