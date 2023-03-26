2fa3c4fe8ad571060de1fbf1ed5b674f6f501fd8
EricvanOs <57840271+EricvanOs@users.noreply.github.com>
2023-03-26 19:07:47 +0200

    ok

---
+++ PS/_CM/T01/E02 - MyInvocation object.ps1
@@ -0,0 +1,25 @@
+function Get-MyComputerDate {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$True,
+                   HelpMessage='Computer name to set state for',
+                   ValueFromPipeline=$True,
+                   ValueFromPipelineByPropertyName=$True)]
+        [Alias('hostname')]
+        [string[]]$ComputerName
+    )
+Begin {
+    Write-Verbose "[$((Get-Date).TimeofDay) BEGIN ] Starting $($myinvocation.mycommand)"
+} #begin
+Process {
+    Write-Verbose "[$((Get-Date).TimeofDay) PROCESS] Processing $computername"
+}
+End {
+    Write-Verbose "[$((Get-Date).TimeofDay) END ] Ending $($myinvocation.mycommand)"
+} #end
+}
+
+# test
+Get-MyComputerDate -ComputerName Amphitrite -Verbose
+
+$MyInvocation | Select-Object -Property *
