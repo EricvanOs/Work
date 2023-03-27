ca6f8fc42ea9c7c9caae52408cdda00680f53930
EricvanOs <57840271+EricvanOs@users.noreply.github.com>
2023-03-27 18:46:33 +0200

    ok

--- PS/_CM/T05/04 - Output XML in fuction.ps1
+++ PS/_CM/T05/04 - Output XML in fuction.ps1
@@ -1,4 +1,54 @@
-﻿# Alternate version uses a parameter
+﻿function Get-AdatumNetAdapterInfo {
+    <#
+    .SYNOPSIS
+    Retrieves network adapter and IP address information.
+    .DESCRIPTION
+    This command combines information about each network adapter
+    and all IP addresses bound to it. This uses CIM, so target computers
+    must have WMF 3.0 or later installed, and WinRM must be enabled.
+    .PARAMETER ComputerName
+    One or more computer name. IP addresses are not acceptable. This
+    parameter does not accept pipeline input.
+    .EXAMPLE
+    Get-AdatumNetAdapterInfo -ComputerName LON-DC1,LON-SVR1
+    #>
+        [CmdletBinding()]
+        param(
+            [Parameter(Mandatory=$True)]
+            [string[]]$ComputerName
+        )
+        
+        foreach ($computer in $computername) {
+    
+            Write-Verbose "Connecting to $computer"
+            $session = New-CimSession -ComputerName $computer
+    
+            $adapters = Get-NetAdapter -CimSession $session
+            foreach ($adapter in $adapters) {
+                
+                $addresses = Get-NetIPAddress -InterfaceIndex ($adapter.InterfaceIndex) -CimSession $session
+                foreach ($address in $addresses) {
+    
+                    $properties = @{'ComputerName'=$computer;
+                                    'AdapterName'=$adapter.Name;
+                                    'InterfaceIndex'=$adapter.InterfaceIndex;
+                                    'IPAddress'=$address.IPAddress;
+                                    'AddressFamily'=$address.AddressFamily}
+                    $output = new-object -TypeName PSObject -Property $properties
+                    Write-Output $output
+    
+                } # addresses
+            } # adapeters
+        } # computers
+    
+        Write-Verbose "Closing session to $computer"
+        $session | Remove-CimSession
+    
+} # function
+
+
+
+# Alternate version uses a parameter
 # to save the new XML directly to a file
 
 
@@ -46,4 +96,7 @@ function Update-NetIPInventory {
 
 }
 
-Update-NetIPInventory -xml (Get-Content C:\inventory.xml) -verbose -OutputPath c:\new-inventory.xml
+Set-Location -Path C:\work\ps\_CM\T05
+
+Update-NetIPInventory -xml (Get-Content .\inventory.xml) -verbose -OutputPath C:\work\ps\_CM\T05\new-inventory.xml
+
