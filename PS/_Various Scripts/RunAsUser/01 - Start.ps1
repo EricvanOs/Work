636b9aa779ab7ec9cea1f6e970748cd6c9e12907
EricvanOs <57840271+EricvanOs@users.noreply.github.com>
2023-03-28 18:15:37 +0200

    ok

---
+++ PS/_Various Scripts/RunAsUser/01 - Start.ps1
@@ -0,0 +1,6 @@
+# see: https://github.com/KelvinTegelaar/RunAsUser
+
+
+<#
+This module has been created to have the ability to run scripts under the current user session while the application executing this script only has SYSTEM access
+#>
