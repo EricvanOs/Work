91a696e4e0e1412910e583630d1223e63020ce99
EricvanOs <57840271+EricvanOs@users.noreply.github.com>
2023-03-28 08:28:39 +0200

    ok

---
+++ PS/_Various Scripts/Piecemeal/01 - Start.ps1
@@ -0,0 +1,13 @@
+<#
+https://github.com/StartAutomating/Piecemeal
+
+
+What is Piecemeal?
+Piecemeal is a little PowerShell module that helps solve a big problem: PowerShell extensibility.
+Piecemeal allows function and scripts to have extended capabilities and act as extensions for any command.
+Piecemeal standardizes these capabilities and provides a simple function you can embed into any module to power an engine of extensibility.
+
+
+#>
+
+
