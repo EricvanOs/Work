c5eb999c0f4f8b8e6771be635f19fd442b459f68
EricvanOs <57840271+EricvanOs@users.noreply.github.com>
2023-03-28 08:16:24 +0200

    ok

---
+++ PS/_Various Scripts/Crescendo/01 - Start Crescendo.ps1
@@ -0,0 +1,16 @@
+<#
+Crescendo is a development accelerator enabling you to rapidly build PowerShell cmdlets that leverage existing command-line tools. 
+Crescendo amplifies the command-line experience of the original tool to include object output for the PowerShell pipeline, 
+privilege elevation, and integrated help information. 
+A Crescendo module replaces cumbersome command-line tools with PowerShell cmdlets that are easier to use in automation and packaged to share with team members.
+
+https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.crescendo/?view=ps-modules
+
+https://devblogs.microsoft.com/powershell-community/tag/crescendo/
+
+https://tommymaynard.com/simple-simple-microsoft-crescendo-example/
+
+https://tommymaynard.com/simple-simple-microsoft-crescendo-example-part-ii/
+
+
+#>
