195835371730c3e85448cab47c33b0cfc120e99b
EricvanOs <57840271+EricvanOs@users.noreply.github.com>
2023-03-29 20:48:58 +0200

    ok

---
+++ PS/_CM/T01/08 - Functions.ps1
@@ -0,0 +1,9 @@
+
+# clean block is in PS7.3 allowed
+
+BEGIN{}
+PROCESS{}
+ENd{}
+CLEAN{}
+
+# https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_functions_advanced_methods?view=powershell-7.3
