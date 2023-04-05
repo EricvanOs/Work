dfef595e3742b1cda15a8716da3f7d730a3969ae
EricvanOs <57840271+EricvanOs@users.noreply.github.com>
2023-04-05 19:29:08 +0200

    ok

--- PS/_Various Scripts/SQL - Write output to SQL Server table/04a- Using JSON.ps1
+++ PS/_Various Scripts/SQL - Write output to SQL Server table/04a- Using JSON.ps1
@@ -3,6 +3,6 @@ $ComputerName = 'echo'
 
 $JSON = Get-CIMInstance win32_logicaldisk -ComputerName $ComputerName -Filter "Drivetype=3"  |
 Select-Object  SystemName,DeviceID,VolumeName,@{Label="Total SIze";Expression={$_.Size / 1gb -as [int] }},@{Label="Free Size";Expression={$_.freespace / 1gb -as [int] }} |
-ConvertTo-Json -AsArray
+ConvertTo-Json 
 
-$json | Out-File -Path $Path -Encoding utf8BOM
+$json | Out-File -Path $Path -Encoding utf8BOM 
