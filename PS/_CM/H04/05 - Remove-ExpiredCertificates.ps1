0f3965646022ec2204a484ce0ce62237ca6581d7
EricvanOs <57840271+EricvanOs@users.noreply.github.com>
2023-04-06 09:18:13 +0200

    ok

---
+++ PS/_CM/H04/05 - Remove-ExpiredCertificates.ps1
@@ -0,0 +1,20 @@
+﻿Push-Location Cert:\LocalMachine\My
+
+Get-ChildItem -recurse |
+Where-Object {$_.NotAfter -lt (Get-Date).Date} | 
+Where-Object{$_.Issuer -like 'CN=pantheon-MORPHEUS-CA*'} | 
+Remove-Item -WhatIf
+
+Pop-Location 
+
+
+<# Just to test 
+Push-Location Cert:\LocalMachine\My
+
+Get-ChildItem -recurse |
+Where-Object {$_.NotAfter -lt [datetime]'2027-12-13'} | 
+Where-Object{$_.Issuer -like 'CN=pantheon-MORPHEUS-CA*'} | 
+Remove-Item -WhatIf
+
+Pop-Location 
+#>
