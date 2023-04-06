5ddd64f426a82e06e1b42ade2d51e64a8652702e
EricvanOs <57840271+EricvanOs@users.noreply.github.com>
2023-04-06 09:07:13 +0200

    ok

--- PS/WorkInProgress/ScriptShare/InvokeRestMethod.ps1
+++
@@ -1,6 +0,0 @@
-﻿$cred = Get-Credential -Credential pantheon\administrator
-
-$response = Invoke-RestMethod 'http://sthenno.pantheon.somewhere:81/Orchestrator2012/Orchestrator.svc/Runbooks' -Credential $cred
-
-
-$response.id
