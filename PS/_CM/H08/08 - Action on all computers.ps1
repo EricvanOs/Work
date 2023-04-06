c674d63013def49fd849f3e352ef50d007516265
EricvanOs <57840271+EricvanOs@users.noreply.github.com>
2023-04-06 10:47:46 +0200

    ok

--- PS/_CM/H08/08 - Action on all computers.ps1
+++ PS/_CM/H08/08 - Action on all computers.ps1
@@ -1,7 +1,7 @@
 ﻿#search for all (recently) active computers
 
 $ses = new-PSSession -ComputerName sisyphus
-$result = invoke-command -session $ses -ScriptBlock{
+$comps = invoke-command -session $ses -ScriptBlock{
     $DaysInactive = 90  
     $time = (Get-Date).Adddays(-($DaysInactive)) 
     $aa = Get-ADComputer -Filter {LastLogonTimeStamp -gt $time} -Properties LastLogonTimeStamp,Description -SearchBase 'dc=pantheon,dc=somewhere' | 
@@ -11,27 +11,14 @@ $result = invoke-command -session $ses -ScriptBlock{
 get-PSSession  | Remove-PSSession
 
 #use output (list of computers)
-$comps = $result
 
-$ErrorActionPreference = 'SilentlyContinue'  # oplossen mbv try and catch
-
-foreach ($comp in $comps){
-  Write-host $comp
-  $ses = New-PSSession -ComputerName $comp
-  Invoke-Command -Session $ses -ScriptBlock {
-      schtasks.exe /delete /TN "QuoteOfDay" /F;
-      schtasks.exe /delete /TN "Mythology" /F
-      }
-  get-PSSession  | Remove-PSSession
-} 
+$ErrorActionPreference = 'SilentlyContinue'  # better use try and catch
 
 foreach ($comp in $comps){
   Write-host $comp
   $ses = New-PSSession -ComputerName $comp
   Invoke-Command -Session $ses -ScriptBlock {
-     Remove-Item -Path 'C:\Utils\BGInfo\Quote.vbs'
-     Remove-item -Path 'C:\Utils\BGInfo\Mythology.vbs'
-     Remove-item -Path 'C:\Utils\BGInfo\Generate_txt.ps1'
+      # Your commands on remote machine
       }
   get-PSSession  | Remove-PSSession
 } 
