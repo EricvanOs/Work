fd62f487b6148473b3705860f240d10d5347851f
EricvanOs <57840271+EricvanOs@users.noreply.github.com>
2023-04-06 13:07:22 +0200

    ok

--- PS/WorkInProgress/3npluseen.ps1
+++ PS/WorkInProgress/3npluseen.ps1
@@ -1,19 +1,29 @@
+function 3nplus1{
+  param (
+    [uint64]$n
+  )
+
 <#
 n = 1  stop
 n even --> n/2
 n oneven --> 3n + 1
 #>
 
-[uint64]$n = 27
-[uint64]$max = $n
+  [uint64]$max = $n
+  [UInt64]$steps = 1
 
-write-host $n
-while ($n -ne 1) {
-  if ($n -ge $max) { $max = $n}  
-   if (($n % 2) -eq 0 ) { $n = $n/2 } 
-   else { $n = 3 * $n + 1  } 
-   write-host $n
-   }
+  write-host $n
+  while ($n -ne 1) {
+    if ($n -ge $max) { $max = $n}  
+    if (($n % 2) -eq 0 ) { $n = $n/2 } 
+    else { $n = 3 * $n + 1  } 
+    $steps += 1
+    write-host $n
+  }
 
-write-host "`r`n"
-write-host "Maximum is $max"
+  write-host "`r`n"
+  write-host "Maximum is $max in $steps steps"
+}
+
+#test
+# 3nPlus1(3)
