2bbf12db74220a6c8dc3b37218b8ccfc17f06528
EricvanOs <57840271+EricvanOs@users.noreply.github.com>
2023-04-08 17:08:07 +0200

    ok

--- PS/WorkInProgress/ChangeOwnerinAzureAD.ps1
+++ PS/WorkInProgress/ChangeOwnerinAzureAD.ps1
@@ -25,4 +25,72 @@ https://docs.microsoft.com/en-us/troubleshoot/azure/active-directory/pending-dev
 #>
 
 
+Disconnect-MgGraph
+
+Connect-MgGraph  -scopes  'User.ReadWrite.All','User.Read.All', 'Device.Read.All' 
+
+$UserId = (get-MgUser | Where-Object{$_.UserPrincipalName -eq 'ericvanos@xhelios.onmicrosoft.com'}).Id
+
+$ObjectId = (Get-MgDevice | Where-Object{$_.Displayname -eq 'mnemosyne'}).Id
+
+Get-MgDevice | Format-List
+
+# add new owner to device
+$params = @{
+	"@odata.id" = "https://graph.microsoft.com/v1.0/directoryObjects/$UserId"
+}
+New-MgDeviceRegisteredOwnerByRef -DeviceId $ObjectId -BodyParameter $params
+
+
+New-MgDeviceRegisteredOwnerByRef -DeviceId $DeviceId -
+
+-ObjectId $objid -RefObjectId $refid
+
+################
+
+Open-HeliosVault
+
+$vault = 'ScriptingVault'
+# Read secrets from vault 
+$tenantId = (Get-Secret -Name TenantID -Vault $vault -AsPlainText) 
+$ApplicationId = (Get-Secret -Name MSGraphUsers-ApplicationID -Vault $vault -AsPlainText) 
+$ApplicationSecret = (Get-Secret -Name MSGraphUsers-Secret -Vault $vault -AsPlainText) 
+
+Close-HeliosVault
+
+
+$openid = Invoke-RestMethod  -uri "https://login.microsoftonline.com/$tenantId/v2.0/.well-known/openid-configuration" 
+$token = $openid.token_endpoint
+
+
+$Body = @{
+    client_id = $ApplicationId      
+    client_secret = $ApplicationSecret      
+    redirect_uri = "https://localhost"
+    grant_type = "client_credentials"
+    scope = "https://graph.microsoft.com/.default" 
+    tenant = $tenantId
+}
+
+# request access token
+$request = Invoke-RestMethod -uri $token -Body $Body -Method Post
+
+
+
+
+
+Connect-MgGraph -AccessToken $request.access_token
+
+
+$UserId = (get-MgUser | Where-Object{$_.UserPrincipalName -eq 'ericvanos@xhelios.onmicrosoft.com'}).Id
+
+$ObjectId = (Get-MgDevice | Where-Object{$_.Displayname -eq 'mnemosyne'}).Id
+
+Get-MgDevice | Format-List
+
+# add new owner to device
+$params = @{
+	"@odata.id" = "https://graph.microsoft.com/v1.0/directoryObjects/$UserId"
+}
+New-MgDeviceRegisteredOwnerByRef -DeviceId $ObjectId -BodyParameter $params
 
