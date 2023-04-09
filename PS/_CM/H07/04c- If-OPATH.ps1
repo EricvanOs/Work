36f6de3df94def46e308f8b5a2e9524ded32cea5
EricvanOs <57840271+EricvanOs@users.noreply.github.com>
2023-04-09 16:34:38 +0200

    ok

--- PS/_CM/H07/04c- If-OPATH.ps1
+++ PS/_CM/H07/04c- If-OPATH.ps1
@@ -19,4 +19,5 @@ $expression -is [string]
 if ( [bool]$expression) {write-host '$a is 4'} else {write-host '$a is not 4 '}
 
 
+# OPATH Queries
 # see: https://techcommunity.microsoft.com/t5/security-compliance-and-identity/using-adaptive-policy-scopes-to-apply-m365-retention-to-shared/ba-p/3053641
