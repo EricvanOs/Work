12d202e10e1bb63b2db8ad95694565aef4b4a456
EricvanOs <57840271+EricvanOs@users.noreply.github.com>
2023-06-02 15:14:32 +0200

    ok

--- PS/_CM/H07B/10-WriteDebug.ps1
+++ PS/_CM/H07B/10-WriteDebug.ps1
@@ -15,7 +15,7 @@
 }
 
 
- Get-FilewithDebug -path C:\Users\administrator.PANTHEON\Downloads -Verbose -Debug
+Get-FilewithDebug -path C:\Users\administrator.PANTHEON\Downloads -Verbose -Debug
 
 # set-location C:\Work\PS\_CM\H07B
- # Suspend, test variables, and exit to continue
+# Suspend, test variables, and exit to continue
