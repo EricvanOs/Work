59cb5e13cc8a10bbc3c50c164cf9b3316d02342a
EricvanOs <57840271+EricvanOs@users.noreply.github.com>
2023-06-04 09:56:29 +0200

    ok

--- PS/_CM/H07/04c- If-OPATH.ps1
+++ PS/_CM/H07/04c- If-OPATH.ps1
@@ -6,18 +6,37 @@ $a = 4
 $expression = $a -eq 4
 if ( $expression) {write-host '$a is 4'} else {write-host '$a is not 4 '}
 
-# of 
+# $expression is not dynamic
+$a = 5
+if ( $expression) {write-host '$a is 4'} else {write-host '$a is not 4 '}
+
+
+# solution, create a dynamic expandable string
 
 $expression = @'
     $a -eq 4
 '@
 
-$expression -is [string]
 
-[bool]$expression -is [bool]
+# test 
+$a = 5
 
-if ( [bool]$expression) {write-host '$a is 4'} else {write-host '$a is not 4 '}
+$ExpandedExpression = $ExecutionContext.InvokeCommand.ExpandString($expression)
+
+Invoke-Expression $ExpandedExpression
+
+# create OPATH expression and use it
+$expression = @'
+    $a -eq 4
+'@
+
+$a = 5
+
+
+if ( Invoke-Expression $ExecutionContext.InvokeCommand.ExpandString($expression) ) {write-host '$a is 4'} else {write-host '$a is not 4 '}
 
 
 # OPATH Queries
 # see: https://techcommunity.microsoft.com/t5/security-compliance-and-identity/using-adaptive-policy-scopes-to-apply-m365-retention-to-shared/ba-p/3053641
+# see: dynamic groups in AAD
+
