3e8a4364f2e4e935b06369f950f20df16c90c338
EricvanOs <57840271+EricvanOs@users.noreply.github.com>
2023-06-04 16:30:02 +0200

    ok

--- PS/_CM/H06/07 - Hashtable_and_Arrays.ps1
+++ PS/_CM/H06/07 - Hashtable_and_Arrays.ps1
@@ -7,12 +7,13 @@ $hashtable = @{
 $array = @("Document", "FileSystem", "C:\Users\victo\Documents")
 $array
 
-$hashtable.Values | Out-string -stream
+$hashtable.Values | Out-string 
 $hashtable.GetEnumerator() |Foreach-Object{ $_ | Out-String }
 
 $LogNameKeys = @{
     LogName = @("System", "Application", "Security")
 }
 #Convert the hashtable to a string
-$LogNameKeys.Values | Out-String -Stream 
+$LogNameKeys.Values | Out-String  
+
 
