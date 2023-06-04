9a695444bf456625175c909edd094e4014cec697
EricvanOs <57840271+EricvanOs@users.noreply.github.com>
2023-06-04 18:57:38 +0200

    ok

--- PS/_CM/H05/CIMCmdlets/01 - Querying_CIM.ps1
+++ PS/_CM/H05/CIMCmdlets/01 - Querying_CIM.ps1
@@ -6,7 +6,7 @@ Get-CimInstance -ClassName Win32_Process
 Get-CimInstance -ClassName Win32_Process | Get-Member
 
 # 3
-Get-CimInstance -ClassName Win32_LogicalDisk -Filter "DriveType=3"
+Get-CimInstance  -ComputerName Epimetheus  -ClassName Win32_LogicalDisk -Filter "DriveType=3"
 
 # 4
 Get-CimInstance -Query "SELECT * FROM Win32_NetworkAdapter"
