2ef7103c2f10ecb4f69b0e88e9c21ec0c367ba6f
EricvanOs <57840271+EricvanOs@users.noreply.github.com>
2023-06-07 14:35:55 +0200

    ok

--- PS/_CM/H07/04c- If-OPATH.ps1
+++ PS/_CM/H07/04c- If-OPATH.ps1
@@ -23,6 +23,8 @@ $a = 5
 
 $ExpandedExpression = $ExecutionContext.InvokeCommand.ExpandString($expression)
 
+#controle
+$ExpandedExpression
 Invoke-Expression $ExpandedExpression
 
 # create OPATH expression and use it
@@ -38,5 +40,5 @@ if ( Invoke-Expression $ExecutionContext.InvokeCommand.ExpandString($expression)
 
 # OPATH Queries
 # see: https://techcommunity.microsoft.com/t5/security-compliance-and-identity/using-adaptive-policy-scopes-to-apply-m365-retention-to-shared/ba-p/3053641
-# see: dynamic groups in AAD
+# see: dynamic groups in AAD e.g. https://portal.azure.com/#view/Microsoft_AAD_IAM/GroupDetailsMenuBlade/~/DynamicGroupMembershipRule/groupId/e6108476-dbf0-43cd-9b36-0c314f35a736
 
