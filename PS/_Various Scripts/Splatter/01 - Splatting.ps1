113667638b4f9e5d136a01fe51850f1dce14eed7
EricvanOs <57840271+EricvanOs@users.noreply.github.com>
2023-06-08 08:09:23 +0200

    ok

--- PS/_Various Scripts/Splatter/01 - Splatting.ps1
+++ PS/_Various Scripts/Splatter/01 - Splatting.ps1
@@ -17,4 +17,31 @@ Import-Module -Name Splatter
     Get-Splat Wait-Process, Stop-Process
 
 
-    @{a='b'}, @{c='d'} | Merge-Splat
+@{a='b'}, @{c='d'} | Merge-Splat
+
+
+# explaination
+
+@{Id=$pid;Junk='Data'} | Get-Splat Get-Process  
+
+$pid #system variable
+
+get-process -id $pid
+
+$Data = @{ID = $pid}
+get-process @Data
+
+$Data = @{
+    ID = $pid
+    Junk = 'Data'
+}
+get-process @Data #fails, too many parameters
+
+get-help get-splat -ShowWindow
+
+Get-Splat -Command Get-Process -Splat @{Id=$pid}
+#or
+@{Id=$pid} | Get-Splat Get-Process     
+#or, discards superfluous parameters
+@{Id=$pid;Junk='Data'} | Get-Splat Get-Process 
+
