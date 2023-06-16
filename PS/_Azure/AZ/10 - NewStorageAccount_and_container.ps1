868cbe20b294b9842493d910b8177538c7f3dcdf
EricvanOs <57840271+EricvanOs@users.noreply.github.com>
2023-06-16 07:50:40 +0200

    ok

--- PS/_Azure/AZ/10 - NewStorageAccount_and_container.ps1
+++ PS/_Azure/AZ/10 - NewStorageAccount_and_container.ps1
@@ -15,17 +15,25 @@ $storage = @{
 if ($null -eq (Get-AzResourceGroup  | Where-Object{$_.ResourceGroupName -eq $ResourceGroupName})) { New-AzResourceGroup -name $ResourceGroupName -Location westeurope }  
 
 # create storage account
-$storageAccount = New-AzStorageAccount @storage -SkuName Standard_LRS  
+$storageAccount = New-AzStorageAccount @storage -SkuName Standard_LRS  -AllowBlobPublicAccess:$false
 
 $ctx = $storageAccount.Context
 
 # Blobs are always uploaded into a container. You can organize groups of blobs the way you organize your files on your computer in folders.
 
 $containerName = "quickstartblobs"
-New-AzStorageContainer -Name $containerName -Context $ctx -Permission blob
+New-AzStorageContainer -Name $containerName -Context $ctx # -Permission blob needed if AllowBlobPublicAccess:$true
 
-# add blob, if needed
-Copy-AzStorageBlob -SrcContainer "sourcecontainername" -SrcBlob "srcblobname" -DestContainer "destcontainername" -DestBlob "destblobname"
+
+# copy storage2he-polybase-customers.csv --> the just created storageaccount-container
+$sourceStorageAccountContext = (Get-AzStorageAccount -ResourceGroupName storage -Name storage2he).Context
+$sourcecontainername = 'polybase'
+$srcblobname = 'Customers.csv'
+$destinationStorageAccountContext = $ctx
+$destcontainername = $containerName
+$destblobname = $srcblobname
+
+Copy-AzStorageBlob -SrcContainer $sourcecontainername -SrcBlob $srcblobname -DestContainer $destcontainername -DestBlob $destblobname  -Context $sourceStorageAccountContext -DestContext $destinationStorageAccountContext
 
 
 # You can use the Set-AzStorageAccount cmdlet to modify an Azure Storage account.
