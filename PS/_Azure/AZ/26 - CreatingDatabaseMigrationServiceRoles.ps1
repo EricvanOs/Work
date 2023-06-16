639d6352bade850fa9aba64348aa278da44ee8eb
EricvanOs <57840271+EricvanOs@users.noreply.github.com>
2023-06-16 09:33:00 +0200

    ok

--- PS/_Azure/AZ/26 - CreatingDatabaseMigrationServiceRoles.ps1
+++ PS/_Azure/AZ/26 - CreatingDatabaseMigrationServiceRoles.ps1
@@ -16,42 +16,42 @@ $writerActions += $readerActions
 $subScopes = "/subscriptions/$subscriptionid/"
 
 function New-DmsReaderRole() {
-$aRole = [Microsoft.Azure.Commands.Resources.Models.Authorization.PSRoleDefinition]::new()
-$aRole.Name = "Azure Database Migration Reader"
-$aRole.Description = "Lets you perform read only actions on DMS service/project/tasks."
-$aRole.IsCustom = $true
-$aRole.Actions = $readerActions
-$aRole.NotActions = @()
-$aRole.AssignableScopes = $subScopes
-#Create the role
-New-AzRoleDefinition -Role $aRole 
+    $aRole = [Microsoft.Azure.Commands.Resources.Models.Authorization.PSRoleDefinition]::new()
+    $aRole.Name = "Azure Database Migration Reader"
+    $aRole.Description = "Lets you perform read only actions on DMS service/project/tasks."
+    $aRole.IsCustom = $true
+    $aRole.Actions = $readerActions
+    $aRole.NotActions = @()
+    $aRole.AssignableScopes = $subScopes
+    #Create the role
+    New-AzRoleDefinition -Role $aRole 
 }
 
 
 function New-DmsContributorRole() {
-$aRole = [Microsoft.Azure.Commands.Resources.Models.Authorization.PSRoleDefinition]::new()
-$aRole.Name = "Azure Database Migration Contributor"
-$aRole.Description = "Lets you perform CRUD actions on DMS service/project/tasks."
-$aRole.IsCustom = $true
-$aRole.Actions = $writerActions
-$aRole.NotActions = @()
-$aRole.AssignableScopes = $subScopes
-#Create the role
-New-AzRoleDefinition -Role $aRole 
+    $aRole = [Microsoft.Azure.Commands.Resources.Models.Authorization.PSRoleDefinition]::new()
+    $aRole.Name = "Azure Database Migration Contributor"
+    $aRole.Description = "Lets you perform CRUD actions on DMS service/project/tasks."
+    $aRole.IsCustom = $true
+    $aRole.Actions = $writerActions
+    $aRole.NotActions = @()
+    $aRole.AssignableScopes = $subScopes
+    #Create the role
+    New-AzRoleDefinition -Role $aRole 
 }
 
 function Update-DmsReaderRole() {
-$aRole = Get-AzRoleDefinition "Azure Database Migration Reader"
-$aRole.Actions = $readerActions
-$aRole.NotActions = @()
-Set-AzRoleDefinition -Role $aRole 
+    $aRole = Get-AzRoleDefinition "Azure Database Migration Reader"
+    $aRole.Actions = $readerActions
+    $aRole.NotActions = @()
+    Set-AzRoleDefinition -Role $aRole 
 }
 
 function Update-DmsConributorRole() {
-$aRole = Get-AzRoleDefinition "Azure Database Migration Contributor"
-$aRole.Actions = $writerActions
-$aRole.NotActions = @()
-Set-AzRoleDefinition -Role $aRole 
+    $aRole = Get-AzRoleDefinition "Azure Database Migration Contributor"
+    $aRole.Actions = $writerActions
+    $aRole.NotActions = @()
+    Set-AzRoleDefinition -Role $aRole 
 }
 
 # invoke the following
