3292cf03aec03e52dae6d5b86c69cc7d2d86f3a8
EricvanOs <57840271+EricvanOs@users.noreply.github.com>
2023-11-26 10:48:08 +0100

    ok

--- PS/_CM/H03B/02c- ByPropertyName.ps1
+++ PS/_CM/H03B/02c- ByPropertyName.ps1
@@ -10,29 +10,32 @@ Get-ADComputer -Filter * | Select-Object -Property name | Get-HotFix
 # want to use name (from get-adcomputer) as computername in get-hotfix
 # create an extra column with correct name
 
-Get-ADComputer -Filter 'Name -like "e*us"' | Select-Object @{n='ComputerName';e={$_.name}} 
+Get-ADComputer -Filter 'Name -like "*o"' | Select-Object @{n='ComputerName';e={$_.name}} 
 
 # now a pipe into get-hotfix
 
-Get-ADComputer -Filter 'Name -like "e*us"' | Select-Object @{n='ComputerName';e={$_.name}} | Get-HotFix
+Get-ADComputer -Filter 'Name -like "*o"' | Select-Object @{n='ComputerName';e={$_.name}} | Get-HotFix 
 
 
 # Verify if an update is installed and if not, write computer name to a file
 # looks as if it doesn't work, but it does, albeit error messages
 
 Set-Location -Path 'C:\Work\PS\_CM\H03B'
-$hotfixid = 'KB4499728'
+$hotfixid = 'KB5004424'
 $servers = Get-Content -Path .\Servers.txt
 $servers | ForEach-Object { if (!(Get-HotFix -Id $hotfixid -ComputerName $_)){ 
     Add-Content $_ -Path .\"Missing-$($hotfixid).txt" }
 }
+
+#delete just created file
+"Missing-$($hotfixid).txt" | Remove-Item 
 
 # test
 Get-HotFix -ComputerName echo -Id xyz
 Get-HotFix -ComputerName echo -Id xyz -ErrorAction SilentlyContinue
 
 # again
-$hotfixid = 'KB4499728'
+$hotfixid = 'KB5004424'
 $servers = Get-Content -Path .\Servers.txt
 $servers | ForEach-Object { if (!(Get-HotFix -Id $hotfixid -ComputerName $_  -ErrorAction SilentlyContinue)){ 
     Add-Content $_ -Path .\"Missing-$($hotfixid).txt" }
