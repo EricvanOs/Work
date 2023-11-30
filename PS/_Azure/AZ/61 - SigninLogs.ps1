f0046201b34d017a61bd51ca7b5bf3a871875781
EricvanOs <57840271+EricvanOs@users.noreply.github.com>
2023-11-30 17:50:41 +0100

    ok

---
+++ PS/_Azure/AZ/61 - SigninLogs.ps1
@@ -0,0 +1,6 @@
+# to be sure
+Disconnect-MgGraph 
+
+Connect-MgGraph -Scopes AuditLog.Read.All
+Get-MgAuditLogSignIn -Top 1 | Select-Object -Property * |
+Select-Object userDisplayName, userPrincipalName, appDisplayName, ipAddress, clientAppUsed, @{Name = 'DeviceOS'; Expression = {$_.DeviceDetail.OperatingSystem}},@{Name = 'Location'; Expression = {$_.Location.City}}
