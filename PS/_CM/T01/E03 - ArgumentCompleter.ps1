44ebb3f68d227ea1b027de81e72b24eac0b8df9e
EricvanOs <57840271+EricvanOs@users.noreply.github.com>
2024-01-15 13:43:26 +0100

    p

--- PS/_CM/T01/E03 - ArgumentCompleter.ps1
+++ PS/_CM/T01/E03 - ArgumentCompleter.ps1
@@ -1,26 +1,27 @@
 Function Get-EventLogDetail {
     [cmdletbinding()]
     Param(
-    [Parameter(Position = 0)]
-    [ArgumentCompleter({(Get-Eventlog -List).log})]
-    [string]$LogName
+      [Parameter(Position = 0)]
+      [ArgumentCompleter({(Get-WinEvent -ListLog *).logname})]
+      [string]$LogName
     )
     Write-Verbose "Getting $LogName log details"
     $params = @{
-    ClassName = "win32_nteventlogfile"
-    filter = "filename='$LogName'"
-    ErrorAction = "Stop"
+      ClassName = "win32_nteventlogfile"
+      filter = "filename='$LogName'"
+      ErrorAction = "Stop"
     }
     Try {
-    Get-CimInstance @params |
-    Select-Object -Property LogFileName, Name, FileSize, MaxFileSize,
-    LastModified, NumberOfRecords,@{Name="PctUsed";
-    Expression={[math]::Round(($_.filesize/$_.maxfilesize)*100,2)}}
+      Get-CimInstance @params |
+        Select-Object -Property LogFileName, Name, FileSize, MaxFileSize,
+        LastModified, NumberOfRecords,@{Name="PctUsed";
+        Expression={[math]::Max(100,[math]::Round(($_.filesize/$_.maxfilesize)*100,2))}}
     }
     Catch {
-    Write-Error $_
+      Write-Error $_
     }
   }
 
 
-# use in powershell 5.1 
+# test
+Get-EventLogDetail -LogName System
