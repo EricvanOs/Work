399a87485f0e25ece6fd8e4534955c4f494ac861
EricvanOs <57840271+EricvanOs@users.noreply.github.com>
2024-01-22 06:13:40 +0100

    ok

--- PS/_CM/T01/C01 - Create Module.ps1
+++ PS/_CM/T01/C01 - Create Module.ps1
@@ -1,7 +1,17 @@
 # see: https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/new-modulemanifest?view=powershell-7.3
 
+<#
+The first time that you run a command from an installed module, PowerShell automatically imports that module. 
+The module must be stored in the locations specified in the $env:PSModulePathenvironment variable. 
+Modules in other locations must be imported using the Import-Module cmdlet.
+#>
 
 Get-Help  New-ModuleManifest  -online
 
 
 $env:PSModulePath
+
+# import-module with verbose
+Import-Module -Name 'C:\PowerShell-Scripts\Modules\HashDB' -Verbose
+# or
+Import-Module -Name HashDB -Verbose
---
+++ PS/_CM/T01/C02b- Using Script Module.ps1
@@ -0,0 +1,6 @@
+Import-Module -Name 'C:\work\PS\_CM\T01\C02a- Script Module.psm1' -Verbose
+
+Get-Help -Name Show-Calendar -ShowWindow
+
+
+Show-Calendar -Start "March, 2010" -End "May, 2010"
